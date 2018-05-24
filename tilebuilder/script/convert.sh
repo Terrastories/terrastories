c25020fd79a8a1910d721c7b531003ad34f603f1
Jason Hinebaugh <jason.hinebaugh@customink.com>
2018-05-24 03:16:19 -0400

    Add docker container for tilebuilder and link to tileserver via docker-compose

---
+++ tilebuilder/script/convert.sh
@@ -0,0 +1,11 @@
+# ogr2ogr -f "GeoJSON" /tmp/geomorph.geojson /data/shapefiles/Suriname_Geomorphology.shp
+# ogr2ogr -f "GeoJSON" /tmp/roads.geojson /data/shapefiles/Suriname_Roads.shp
+# ogr2ogr -f "GeoJSON" /tmp/creeks.geojson /data/shapefiles/Saramacca_Creeks.shp
+# tippecanoe -o /data/mbtiles/basic.mbtiles /tmp/geomorph.geojson /tmp/roads.geojson /tmp/creeks.geojson
+
+find /data/shapefiles | grep ".shp$" | while read filepath; do
+  file=$(echo "$filepath" | sed -E 's@.*/([0-9a-zA-Z_-]+).[a-z]+@\1@')
+  ogr2ogr -f "GeoJSON" /tmp/$file.json $filepath
+done
+
+tippecanoe -o /data/mbtiles/basic.mbtiles $(find /tmp -type f | grep .json$)
