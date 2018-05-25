ef6f073a0c5780e48c432ece601837b3c5bc507b
Jason Hinebaugh <jason.hinebaugh@customink.com>
2018-05-25 16:43:24 -0400

    Add line limited jq step back into conversion script

--- tilebuilder/script/convert.sh
+++ tilebuilder/script/convert.sh
@@ -3,9 +3,13 @@
 # ogr2ogr -f "GeoJSON" /tmp/creeks.geojson /data/shapefiles/Saramacca_Creeks.shp
 # tippecanoe -o /data/mbtiles/basic.mbtiles /tmp/geomorph.geojson /tmp/roads.geojson /tmp/creeks.geojson
 
+mkdir /tmp/geo
+mkdir /tmp/line
+
 find /data/shapefiles | grep ".shp$" | while read filepath; do
   file=$(echo "$filepath" | sed -E 's@.*/([0-9a-zA-Z_-]+)\.[a-z]+@\1@')
-  ogr2ogr -f "GeoJSON" /tmp/$file.json $filepath
+  ogr2ogr -f "GeoJSON" /tmp/geo/$file.json $filepath
+  jq -rc '.features[]' /tmp/geo/$file.json > /tmp/line/$file.geojson
 done
 
-tippecanoe -o /data/mbtiles/basic.mbtiles $(find /tmp -type f | grep .json$)
+tippecanoe -o /data/mbtiles/basic.mbtiles $(find /tmp/line -type f | grep .json$)
