6dffe2edfe12f876c789ce539f6f4c49628e26bc
liz <lizanderson.ca@gmail.com>
2018-06-08 14:48:54 -0400

    define point model
    define and migrate point model

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_08_140457) do
+ActiveRecord::Schema.define(version: 2018_06_08_180831) do
+
+  create_table "points", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title"
+    t.string "story"
+    t.decimal "lng", precision: 10
+    t.decimal "lat", precision: 10
+    t.string "location_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
