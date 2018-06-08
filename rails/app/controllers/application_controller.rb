4b0d61ae410e155f0863612a08d55398d9227152
Marc Bellingrath <marrrc.b@gmail.com>
2018-06-08 00:50:39 -0400

    Added pundit and rspec.

--- rails/app/controllers/application_controller.rb
+++ rails/app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  include Pundit
+  protect_from_forgery
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to(root_path)
+  end
 end
