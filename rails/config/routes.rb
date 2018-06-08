82f9fa9327c3688481c27f329fb8cfbba66f51b2
Sean Linsley <code@seanlinsley.com>
2018-06-08 09:23:14 -0500

    add modal when click on points on the map

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
+  get  'point' => 'home#show'
+  post 'point' => 'home#create'
 end
