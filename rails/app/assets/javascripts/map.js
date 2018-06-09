2f8999dce04a3bf5b6c1bae9a1aedf3f2a498d4d
smurfik <tammy@mlpinit.com>
2018-06-09 17:03:35 -0400

    resolve merge conflicts

--- rails/app/assets/javascripts/map.js
+++ rails/app/assets/javascripts/map.js
@@ -4,7 +4,6 @@ $(document).ready(function() {
     [-60.80409032, 0.3332811], //southwest
     [-52.41053563, 6.90258397] //northeast
   ]
-
   var map = new mapboxgl.Map({
       container: 'map', // container id
       style: '/tiles/styles/basic/style.json',
@@ -12,65 +11,27 @@ $(document).ready(function() {
       zoom: 7.6, // starting zoom
       maxBounds: bounds
   });
-
-
-  var modal = document.querySelector('#modal');
-
-  map.on('click', function(event) {
+  map.on('load', function () {
     Rails.ajax({
-      url: '/point?lat=' + event.lngLat.lat + '&long=' + event.lngLat.lng,
+      url: '/points',
       type: 'GET',
-      success: function(_, _, xhr) {
-        modal.getElementsByClassName('modal-guts')[0].innerHTML = xhr.response;
-        modal.classList.toggle('closed');
+      dataType: 'json',
+      success: function(response) {
+        response.features.forEach(function(marker) {
+          console.log(marker.properties);
+          // create a HTML element for each feature
+          var el = document.createElement('div');
+          el.className = 'marker';
+          var popup = new mapboxgl
+            .Popup({ offset: 15 })
+            .setHTML('<h1>' + marker.properties.title + '</h1>' + '<h2>' + marker.properties.region + '</h2>')
+          // make a marker for each feature and add to the map
+          new mapboxgl.Marker(el)
+          .setLngLat(marker.geometry.coordinates)
+          .setPopup(popup) // sets a popup on this marker
+          .addTo(map);
+        });
       }
     });
-  });
-
-  map.on('load', function () {
-    var geojson = {
-      type: 'FeatureCollection',
-      features: [{
-        type: 'Feature',
-        geometry: {
-          type: 'Point',
-          coordinates: [-55.370960068932845, 4.412754499901737]
-        },
-        properties: {
-          title: 'Mapbox',
-          description: 'Washington, D.C.'
-        }
-      },
-      {
-        type: 'Feature',
-        geometry: {
-          type: 'Point',
-          coordinates: [-57.38235594732383, 4.517535458793745]
-        },
-        properties: {
-          title: 'Mapbox',
-          description: 'San Francisco, California'
-        }
-      }]
-    };
-
-    // add markers to map
-    geojson.features.forEach(function(marker) {
-
-      // create a HTML element for each feature
-      var el = document.createElement('div');
-      el.className = 'marker';
-
-      // make a marker for each feature and add to the map
-      new mapboxgl.Marker(el)
-      .setLngLat(marker.geometry.coordinates)
-      .addTo(map);
-    });
-  });
-
-  var closeButton = document.querySelector('#close-button');
-
-  closeButton.addEventListener('click', function() {
-    modal.classList.toggle('closed');
   });
 });
