9cd49dd7d76b8c2d03a70b223de71c276774cae2
Sebastian Caso <scaso@knightpoint.com>
2018-06-09 17:35:06 -0400

    insert test html5 player

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2018_06_09_192407) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
