01b8b75d7f38559ebf5e7a90e9c24f0727a7a308
Tammy Pop <tammy@mlpinit.com>
2018-06-18 10:06:00 -0500

    #61 add home page (#68)

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   resources :stories
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'home#index'
+  root to: 'welcome#index'
+  get 'home', to: 'home#index', as: "home_map"
   resources :points
   resources :speakers
 end
