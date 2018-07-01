baba7f55af60bbaff15e1a1990fab1d0011fe270
Jason Hinebaugh <jason.hinebaugh@customink.com>
2018-07-01 16:16:14 -0400

    [73] Prove that a React component will render

---
+++ rails/app/javascript/components/butterfly.jsx
@@ -0,0 +1,11 @@
+import React, {PureComponent} from 'react';
+
+class Butterfly extends PureComponent {
+  render() {
+    return (
+      <div>Is this a …component?</div>
+    );
+  }
+}
+
+export default Butterfly;
--- rails/app/javascript/packs/application.jsx
+++ rails/app/javascript/packs/application.jsx
@@ -7,4 +7,9 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Butterfly from 'components/butterfly';
+
+ReactDOM.render(<Butterfly />, document.getElementById('react-root'));
