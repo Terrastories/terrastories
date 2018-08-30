b7a208da8bbc71513c2d0528028dff926c155fe1
nickmjones <hello@nickmjones.me>
2018-08-30 09:14:15 -0400

    Adds popup component for intro - Moves the intro content out of the sidecard and into its own component. Allows the sidebar card more space for functionality.

---
+++ rails/app/javascript/components/IntroPopup.jsx
@@ -0,0 +1,34 @@
+import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
+
+class IntroPopup extends PureComponent {
+
+  constructor(props){
+    super(props);
+    this.state = { isPopped: true }
+    this.handleIntroPopup = this.handleIntroPopup.bind(this);
+  }
+
+  handleIntroPopup(){
+    this.setState(prevState =>({
+      isPopped: !prevState.isPopped
+    }));
+  }
+
+  render(){
+    return(
+      <div className={this.state.isPopped ? 'intro-card isShown' : 'intro-card isHidden'}>
+        <h2>Introduction</h2>
+        <h3>What's a Terrastory?</h3>
+        <p>Terrastories are audiovisual recordings of place-based storytelling. This offline-compatible application enables local communities to locate and map their own oral storytelling traditions about places of significant meaning or value to them.</p>
+
+        <div className="intro-card--actions">
+          <span class="count" onClick={this.handleIntroPopup.bind('intro-card isHidden')}>Close</span>
+        </div>
+      </div>
+    );
+  }
+
+}
+
+export default IntroPopup;
