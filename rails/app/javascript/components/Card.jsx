d7435d6e00859ff2648a16c464f50e28a5c9c50b
nickmjones <hello@nickmjones.me>
2018-08-29 20:42:43 -0400

    Rewrite of card markup and CSS. - Minimizes distractions between the card vs. the map - Removes the need to make certain parts of the card sticky, since everything important is in view all the time - More fleshed out styling for stories - Typographic improvements for all page elements, including a rudimentary modular scale for headings and paragraphs - Addition of search form affordance (not yet wired up to anything) - Ability to hide and show the card to allow the user to concentrate on the map when desired. - There may be more, but these are the big tickets.

--- rails/app/javascript/components/Card.jsx
+++ rails/app/javascript/components/Card.jsx
@@ -2,60 +2,47 @@ import React, { PureComponent } from "react";
 import StoryList from "./StoryList";
 
 class Card extends PureComponent {
+
+  constructor(props){
+    super(props);
+    this.state = { isToggleOn: true };
+    this.handleTray = this.handleTray.bind(this);
+  }
+
+  handleTray() {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+  }
+
   render() {
     return (
-      <div className="card">
-        <div className="logo">
-          <img src='assets/logocombo.svg' alt="Terrastories" />
+        <div className={this.state.isToggleOn ? 'card onCanvas' : 'card offCanvas'} >
+        <div className="tab" onClick={this.handleTray.bind('card offCanvas')}>&#9658;</div>
+        <div className="card--logo">
+          <img src="assets/logocombo.svg" alt="Terrastories" />
         </div>
-        <div className="intro">
-          <h1>Introduction</h1>
-          <h3>What's a Terrastory?</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </p>
-        </div>
+ 
 
-        <nav>
+        <form name="search" className="card--search" method="post">
+          <input type="search" placeholder="Search Terrastories" className="search--field"></input>
+        </form>
+
+        <div className="card--nav">
           <ul>
-            <li className="search">
-              <svg
-                viewBox="0 0 485.213 485.213"
-                style={{
-                  enableBackground: "new 0 0 485.213 485.213",
-                  width: "30px",
-                  height: "25px",
-                  fill: "rgb(254,184,48)"
-                }}
-              >
-                <g>
-                  <path
-                    d="M363.909,181.955C363.909,81.473,282.44,0,181.956,0C81.474,0,0.001,81.473,0.001,181.955s81.473,181.951,181.955,181.951
-                    C282.44,363.906,363.909,282.437,363.909,181.955z M181.956,318.416c-75.252,0-136.465-61.208-136.465-136.46
-                    c0-75.252,61.213-136.465,136.465-136.465c75.25,0,136.468,61.213,136.468,136.465
-                    C318.424,257.208,257.206,318.416,181.956,318.416z"
-                  />
-                  <path
-                    d="M471.882,407.567L360.567,296.243c-16.586,25.795-38.536,47.734-64.331,64.321l111.324,111.324
-                    c17.772,17.768,46.587,17.768,64.321,0C489.654,454.149,489.654,425.334,471.882,407.567z"
-                  />
-                </g>
-              </svg>
-            </li>
-            <li>
-              <a href="#">Filter 1 ⌄</a>
-            </li>
-            <li>
-              <a href="#">Filter 2 ⌄</a>
-            </li>
-            <li>
-              <a href="#">Filter 3 ⌄</a>
-            </li>
+            <li><a href="#">Filter 1 ⌄</a></li>
+            <li><a href="#">Filter 2 ⌄</a></li>
+            <li><a href="#">Filter 3 ⌄</a></li>
           </ul>
-        </nav>
+        </div>
 
         <StoryList stories={this.props.stories} />
+
+        <div className="card--tasks">
+          <ul>
+            <li>Hello admin@terrastories.net (<a href="#">Logout</a>)</li>
+          </ul>
+        </div>
       </div>
     );
   }
