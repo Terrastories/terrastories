16261a22c505c42df20dcd2f47fb427982d4a05a
Marc Bellingrath <marrrc.b@gmail.com>
2018-09-09 12:01:07 -0400

    Fix speaker image display and uploads.

--- rails/app/models/speaker.rb
+++ rails/app/models/speaker.rb
@@ -1,4 +1,12 @@
 class Speaker < ApplicationRecord
   has_many :stories
   has_one_attached :media
+
+  def picture_url
+    if media.attached?
+      Rails.application.routes.url_helpers.rails_blob_path(media, only_path: true)
+    else
+      ActionController::Base.helpers.image_path('speaker.png', only_path: true)
+    end
+  end
 end
