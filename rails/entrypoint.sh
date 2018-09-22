d65c93093695300da2689aaf5041aaae1ffa4619
Jason Hinebaugh <jason.hinebaugh@customink.com>
2018-09-22 13:03:49 -0400

    Nevermind, do copy clean assets from image each time the container starts

--- rails/entrypoint.sh
+++ rails/entrypoint.sh
@@ -1,15 +1,19 @@
 echo "Removing existing yarn modules from host"
 rm -rf /app/node_modules /app/yarn.lock
+
 echo "Copying container yarn modules"
-mv /node_modules /app/node_modules
+cp -rf /node_modules /app/node_modules
+cp -rf /yarn.lock /app/yarn.lock
+
 echo "Removing artifacts from previous run"
-mv /yarn.lock /app/yarn.lock
-rm -f /app/tmp/pids/server.pid
+rm -rf /app/tmp
+rm -rf /app/log
 
 if echo "ActiveRecord::Base.logger = nil; ActiveRecord::Base.connection.tables" | bundle exec rails console | grep schema_migrations 2>&1 > /dev/null \
 ; then
   echo "Running pending migrations"
   bundle exec rails db:migrate
+
 else
   echo "Performing first-time setup"
   bundle exec rails db:setup
