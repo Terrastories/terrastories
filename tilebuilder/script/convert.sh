2f579a97b227db930a6b68fb1a7cfa6e57974018
rudokemper <rkemper@amazonteam.org>
2018-10-02 18:45:13 -0300

    Small design and i18n tweaks
    *updated markers
    
    *logo for the organizations that worked on this (Matawai) project
    
    * small CSS tweaks
    
    * updated .sh scripts in UNIX format

--- tilebuilder/script/convert.sh
+++ tilebuilder/script/convert.sh
@@ -1,19 +1,11 @@
-# ogr2ogr -f "GeoJSON" /tmp/geomorph.geojson /data/shapefiles/Suriname_Geomorphology.shp
-# ogr2ogr -f "GeoJSON" /tmp/roads.geojson /data/shapefiles/Suriname_Roads.shp
-# ogr2ogr -f "GeoJSON" /tmp/creeks.geojson /data/shapefiles/Saramacca_Creeks.shp
-# tippecanoe -o /data/mbtiles/basic.mbtiles /tmp/geomorph.geojson /tmp/roads.geojson /tmp/creeks.geojson
-
 mkdir /tmp/geo
 mkdir /tmp/line
-
 rm -rf /data/mbtiles/*
 rm -rf /tmp/geo/*
 rm -rf /tmp/line/*
-
 find /data/shapefiles | grep ".shp$" | while read filepath; do
   file=$(echo "$filepath" | sed -E 's@.*/([0-9a-zA-Z_-]+)\.[a-z]+@\1@')
   ogr2ogr -f "GeoJSON" /tmp/geo/$file.json $filepath
   jq -rc '.features[]' /tmp/geo/$file.json > /tmp/line/$file.geojson
 done
-
 tippecanoe --read-parallel -z 13 -o /data/mbtiles/basic.mbtiles $(find /tmp/line -type f | grep .json$)
