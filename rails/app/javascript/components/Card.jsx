142c52fdf9dcf2bc296c842dee352441d4432d65
Miranda Wang <mirandawang94@gmail.com>
2018-10-01 22:09:22 -0400

    Replace broken logout link with link back to welcome page for now

--- rails/app/javascript/components/Card.jsx
+++ rails/app/javascript/components/Card.jsx
@@ -27,14 +27,14 @@ class Card extends Component {
     if (this.props.user && this.props.user.role === 'editor') {
       return (
         <ul>
-          <li>Hello {this.props.user.email} (<a href="/users/sign_out">Logout</a>)</li>
+          <li>Hello {this.props.user.email} (<a href="/">Back to Welcome Page</a>)</li>
           <li><a href="/admin">Admin Page</a></li>
         </ul>
       );
     } else if (this.props.user) {
       return (
         <ul>
-          <li>Hello {this.props.user.email} (<a href="/users/sign_out">Logout</a>)</li>
+          <li>Hello {this.props.user.email} (<a href="/">Back to Welcome Page</a>)</li>
         </ul>
       );
     }
