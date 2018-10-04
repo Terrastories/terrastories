0169fcb70f8ae70d3b0575864fe923af80fbf745
Miranda Wang <mirandawang94@gmail.com>
2018-10-03 21:54:41 -0400

    Get stories working with lazy loading React Virtualized List

--- rails/app/javascript/components/StoryList.jsx
+++ rails/app/javascript/components/StoryList.jsx
@@ -1,37 +1,53 @@
 import React, { Component  } from "react";
 import PropTypes from "prop-types";
 import StoryMedia from "./StoryMedia";
+import {AutoSizer} from 'react-virtualized/dist/commonjs/AutoSizer';
+import {List} from 'react-virtualized/dist/commonjs/List';
 
 class StoryList extends Component {
   static propTypes = {
     stories: PropTypes.array
   };
 
+  renderStory = ({key, index, style}) => {
+    const story = this.props.stories[index];
+    return (
+        <li
+          className={`story storypoint${story.point && story.point.id}`}
+          onClick={_ => this.props.onStoryClick([story.point.lng, story.point.lat])}
+          key={key}
+          style={style}
+        >
+          <div className="speakers">
+            <img src={story.speaker.picture_url} alt={story.speaker.name} title={story.speaker.name}/>
+          </div>
+          <div className="container">
+            <h6 className="title">{story.title}</h6>
+            <p>{story.desc}</p>
+            {story.media &&
+              story.media.map(file => <StoryMedia file={file} />)}
+          </div>
+        </li>
+    );
+  };
 
   render() {
     return (
       <div className="stories">
-        <ul>
-          {this.props.stories.map(story => {
-            const {coordinates} = story.point.geometry;
+        <AutoSizer>
+          {({height, width}) => {
             return (
-              <li
-                className={`story storypoint${story.point && story.point.id}`}
-                onClick={_ => this.props.onStoryClick(coordinates)}
-              >
-                <div className="speakers">
-                  <img src={story.speaker.picture_url} alt={story.speaker.name} title={story.speaker.name}/>
-                </div>
-                <div className="container">
-                  <h6 className="title">{story.title}</h6>
-                  <p>{story.desc}</p>
-                  {story.media &&
-                    story.media.map(file => <StoryMedia file={file} />)}
-                </div>
-              </li>
+              <List
+                height={height}
+                width={width}
+                rowCount={this.props.stories.length}
+                rowHeight={height}
+                rowRenderer={this.renderStory}
+                overscanRowCount={1}
+              />
             );
-          })}
-        </ul>
+          }}
+        </AutoSizer>
       </div>
     );
   }
