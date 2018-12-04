f5015cb15ea645d649e99e7767432c0366589036
Jason Hinebaugh <jason.hinebaugh@customink.com>
2018-12-04 18:19:18 -0500

    Cleanup debug and log statements

--- rails/app/policies/story_policy.rb
+++ rails/app/policies/story_policy.rb
@@ -10,18 +10,14 @@ class StoryPolicy
         def resolve
             Rails.logger.debug "User: #{user}"
             if user&.admin?
-                Rails.logger.debug 'Admin user gets all stories'
                 Story.all
             elsif user
-                Rails.logger.debug 'Known user gets stories limited to demographics'
                 demographics_ids = user.demographic.map(&:id)
                 Story.joins(:demographic).where([
                   "`demographics`.`id` = :demo OR `permission_level` = :anon",
                   {demo: demographics_ids, anon: :anonymous}
                 ])
-                # Story.joins(:demographic).where('demographics.id': demographics_ids)
             else
-                Rails.logger.debug 'Anonymous user gets only public stories'
                 Story.where(permission_level: :anonymous)
             end
         end
