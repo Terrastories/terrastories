1a4147ecf0fb96bd641e7c1056605374e550df00
Barbara Perlik <barbperlik@gmail.com>
2019-02-08 20:25:28 -0500

    Add-birthplace-and-birth_date

--- rails/app/models/speaker.rb
+++ rails/app/models/speaker.rb
@@ -1,8 +1,24 @@
+# == Schema Information ==
+#
+# Table name: speakers
+# 
+# id          ... not null primary key
+# speaker_name... string
+# birth_year  ... datetime, nil if blank
+# birthplace  ... string, classname: place
+# photo       ... string, url to attached media
+# region ------ removed
+# community --- removed
+# created_at  ... datetime, not null
+# updated_at  ... datetime, not null
+
 class Speaker < ApplicationRecord
   has_many :speaker_stories
   has_many :stories, through: :speaker_stories
+  has_one :birthplace, class_name: Place
   has_one_attached :media
 
+  # photo
   def picture_url
     if media.attached?
       Rails.application.routes.url_helpers.rails_blob_path(media, only_path: true)
@@ -13,13 +29,16 @@ class Speaker < ApplicationRecord
 
   def self.import_csv(filename)
     CSV.parse(filename, headers: true) do |row|
-      speaker = Speaker.where(name: row[0], community: row[2]).first_or_create
+      speaker = Speaker.where(
+        name: row[0], 
+        birth_year: row[1].nil? ? nil : Date.parse(row[1]), 
+        birthplace: Place.find_by(name: row[2])).first_or_create
+
       if row[3] && File.exist?(Rails.root.join('media', row[3]))
         file = File.open(Rails.root.join('media',row[3]))
         speaker.media.attach(io: file, filename: row[3])
+      
         speaker.save
-      end
     end
   end
-
 end
