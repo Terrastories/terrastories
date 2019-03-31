4659150ceee799367c62dfaea8e1d7d9539aa356
Corinne Henk <corinne.m.henk@gmail.com>
2019-03-31 09:12:56 -0700

    Don't reload map to reload points

--- rails/app/javascript/components/Map.jsx
+++ rails/app/javascript/components/Map.jsx
@@ -7,7 +7,7 @@ export default class Map extends Component {
     mapboxgl.accessToken = this.props.mapboxAccessToken;
   }
 
-  componentDidUpdate() {
+  componentDidMount() {
     // @NOTE: MAKE SURE ARRAY IS [LONGITUDE, LATITUDE]
     const bounds = [
       [-60.80409032, 0.3332811], //southwest
@@ -22,35 +22,44 @@ export default class Map extends Component {
       maxBounds: bounds
     });
 
-    this.map.addControl(new mapboxgl.NavigationControl());
-    if(this.props.points) {
-      this.map.on('load', () => {
-        // just testing the passing in of the coords
-        this.props.points.features.forEach(marker => {
-          (marker.properties);
-           // create a HTML element for each feature
-           var el = document.createElement('div');
-           el.className = 'marker';
-           el.id = 'storypoint' + marker.id;
-           var popup = new mapboxgl
-             .Popup({ offset: 15 })
-             .setHTML('<h1>' + marker.properties.title + '</h1>' + '<h2>' + marker.properties.region + '</h2>')
-           // make a marker for each feature and add to the map
-           new mapboxgl.Marker(el)
-           .setLngLat(marker.geometry.coordinates)
-           .setPopup(popup) // sets a popup on this marker
-           .addTo(this.map);
+    this.map.on('load', () => {
+      this.updateMarkers();
+    });
 
-           el.addEventListener('click', () =>
-           {
-             this.props.onMapPointClick(marker.properties.stories);
-             // this.map.setZoom({zoom: 1})
-             this.map.panTo(marker.geometry.coordinates);
-           }
-         )
-        });
-      });
-    }
+    this.map.addControl(new mapboxgl.NavigationControl());
+  }
+
+  updateMarkers() {
+    this.props.points.features.forEach(marker => {
+      (marker.properties);
+       // create a HTML element for each feature
+       var el = document.createElement('div');
+       el.className = 'marker';
+       el.id = 'storypoint' + marker.id;
+       var popup = new mapboxgl
+         .Popup({ offset: 15 })
+         .setHTML('<h1>' + marker.properties.title + '</h1>' + '<h2>' + marker.properties.region + '</h2>')
+       // make a marker for each feature and add to the map
+       new mapboxgl.Marker(el)
+       .setLngLat(marker.geometry.coordinates)
+       .setPopup(popup) // sets a popup on this marker
+       .addTo(this.map);
+
+       el.addEventListener('click', () =>
+         {
+           this.props.onMapPointClick(marker.properties.stories);
+           this.map.panTo(marker.geometry.coordinates);
+         }
+       )
+    });
+  }
+
+  componentDidUpdate() {
+    [...document.querySelectorAll('.marker')].forEach(function(marker) {
+      marker.remove()
+    });
+
+    this.updateMarkers();
 
     if (this.props.pointCoords.length  > 0) {
       if (this.map) {
