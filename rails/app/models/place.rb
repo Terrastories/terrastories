48980132460a7e8d85b40628c88abc984938fcde
Corinne Henk <corinne.m.henk@gmail.com>
2019-03-31 16:48:31 -0700

    Remove linebreak

--- rails/app/models/place.rb
+++ rails/app/models/place.rb
@@ -8,8 +8,7 @@ class Place < ApplicationRecord
   def self.import_csv(filename)
     CSV.parse(filename, headers: true) do |row|
       loc = Place.where(name: row[0], type_of_place: row[1]).first_or_create
-      loc.points.create(title:row[0], lat: row[5].to_f, lng: row[4].to_f,
-      region: row[3] )
+      loc.points.create(title:row[0], lat: row[5].to_f, lng: row[4].to_f, region: row[3])
     end
   end
 
