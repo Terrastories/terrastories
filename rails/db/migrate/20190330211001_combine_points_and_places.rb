dd3eecff480e1c8a1647f7b8d4f8529dda64c4b4
Kelly Sousa <kellysousa1920@gmail.com>
2019-03-31 15:23:43 -0700

    address PR feedback w corinne

--- rails/db/migrate/20190330211001_combine_points_and_places.rb
+++ rails/db/migrate/20190330211001_combine_points_and_places.rb
@@ -1,13 +1,9 @@
 class CombinePointsAndPlaces < ActiveRecord::Migration[5.2]
   def up
-    add_column :places, :description, :string
-    add_column :places, :region, :string
     add_column :places, :lat, :float
     add_column :places, :long, :float
   end
   def down
-    remove_column :places, :description
-    remove_column :places, :region
     remove_column :places, :lat
     remove_column :places, :long
   end
--- rails/db/migrate/20190331181118_convert_point_to_place_id.rb
+++ rails/db/migrate/20190331181118_convert_point_to_place_id.rb
@@ -1,11 +1,13 @@
 class ConvertPointToPlaceId < ActiveRecord::Migration[5.2]
   def change
     points = Point.all.index_by(&:place_id)
-      Place.find_each do |place|
-        point = points[place.id]
-        place.update!(
-          lat: point.lat,
-          lng: point.lng
-        )
+
+    Place.find_each do |place|
+      point = points[place.id]
+      place.update!(
+        lat: point.lat,
+        lng: point.lng
+      )
+    end
   end
 end
