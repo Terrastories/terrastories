1d4ea295483fd023beaf5aeef28d276e3ac3723e
Kalimar Maia <kalimar.maia@gmail.com>
2019-04-17 15:17:43 -0400

    DO NOT MERGE - just showing what is required to have Atlas serve tiles

--- rails/app/javascript/components/Map.jsx
+++ rails/app/javascript/components/Map.jsx
@@ -4,7 +4,9 @@ import React, { Component } from 'react';
 export default class Map extends Component {
   constructor(props) {
     super(props);
-    mapboxgl.accessToken = this.props.mapboxAccessToken;
+    // mapboxgl.accessToken = this.props.mapboxAccessToken;
+    mapboxgl.accessToken = "pk.eyJ1IjoiYXRsYXMtdXNlciIsImEiOiJjam9yNW5sNGgwMDAwMGhuNWZmNDU2dzdzIn0.H-BNTbjyiPZ7qNl1NM1U-A"
+    mapboxgl.config.API_URL = "https://localhost:9021/" // needed for running atlas. This is assumed when using apbox.com
   }
 
   state = {
@@ -12,9 +14,9 @@ export default class Map extends Component {
   }
 
   componentDidUpdate() {
-    if (this.props.pointCoords.length  > 0) {
+    if (this.props.pointCoords.length > 0) {
       if (this.map) {
-        this.map.flyTo({center: this.props.pointCoords, zoom: 14});
+        this.map.flyTo({ center: this.props.pointCoords, zoom: 14 });
       }
     }
   }
@@ -22,42 +24,42 @@ export default class Map extends Component {
   componentDidMount() {
     // @NOTE: MAKE SURE ARRAY IS [LONGITUDE, LATITUDE]
     const bounds = [
-      [-60.80409032, 0.3332811], //southwest
-      [-52.41053563, 6.90258397] //northeast
+      [-180, -85], //southwest
+      [180, 85] //northeast
     ]
 
     this.map = new mapboxgl.Map({
       container: this.mapContainer,
-      style: this.props.mapboxStyle,
-      center: [-55.63, 4.78],
-      zoom: 7.6,
+      // style: this.props.mapboxStyle,
+      style: "mapbox://styles/atlas-user/cjpvc5ane002y01mw9cv9yu8j",
+      center: [-108, 38.5],
+      zoom: 3.5,
       maxBounds: bounds
     });
     this.map.addControl(new mapboxgl.NavigationControl());
-    if(this.props.points) {
+    if (this.props.points) {
       this.map.on('load', () => {
         // just testing the passing in of the coords
         this.props.points.features.forEach(marker => {
           (marker.properties);
-           // create a HTML element for each feature
-           var el = document.createElement('div');
-           el.className = 'marker';
-           el.id = 'storypoint' + marker.id;
-           var popup = new mapboxgl
-             .Popup({ offset: 15 })
-             .setHTML('<h1>' + marker.properties.title + '</h1>' + '<h2>' + marker.properties.region + '</h2>')
-           // make a marker for each feature and add to the map
-           new mapboxgl.Marker(el)
-           .setLngLat(marker.geometry.coordinates)
-           .setPopup(popup) // sets a popup on this marker
-           .addTo(this.map);
+          // create a HTML element for each feature
+          var el = document.createElement('div');
+          el.className = 'marker';
+          el.id = 'storypoint' + marker.id;
+          var popup = new mapboxgl
+            .Popup({ offset: 15 })
+            .setHTML('<h1>' + marker.properties.title + '</h1>' + '<h2>' + marker.properties.region + '</h2>')
+          // make a marker for each feature and add to the map
+          new mapboxgl.Marker(el)
+            .setLngLat(marker.geometry.coordinates)
+            .setPopup(popup) // sets a popup on this marker
+            .addTo(this.map);
 
-           el.addEventListener('click', () =>
-           {
-             $(".story").hide();
-             $(".story." + el.id).show();
-           }
-         )
+          el.addEventListener('click', () => {
+            $(".story").hide();
+            $(".story." + el.id).show();
+          }
+          )
         });
       });
     }
@@ -67,7 +69,7 @@ export default class Map extends Component {
       <div ref={
         el => this.mapContainer = el
       }
-      className = "ts-MainMap"/>
+        className="ts-MainMap" />
     );
   }
 }
