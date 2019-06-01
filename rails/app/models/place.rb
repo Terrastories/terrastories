fcfc120c63b47ffc98517cb64f59c6ec4734bfd2
Mary Katherine McKenzie <mary_katherine.mckenzie@stitchfix.com>
2019-06-01 13:50:23 -0700

    relate stories and places

--- rails/app/models/place.rb
+++ rails/app/models/place.rb
@@ -1,14 +1,19 @@
 class Place < ApplicationRecord
   require 'csv'
   has_many :points
-  has_many :speakers
-
+  has_and_belongs_to_many :stories
   has_one_attached :photo
   validate :photo_format
 
+  acts_as_taggable
+
+  attr_reader :point_geojson
+
   def self.import_csv(filename)
     CSV.parse(filename, headers: true) do |row|
-      loc = Place.where(name: row[0], type_of_place: row[1]).first_or_create
+      place = Place.create(name: row[0], type_of_place: row[1],lat: row[5].to_f, long: row[4].to_f, region: row[3])
+      # keep existing points creation in case of a rollback
+      loc = Place.find_by(name: row[0], type_of_place: row[1])
       loc.points.create(title:row[0], lat: row[5].to_f, lng: row[4].to_f, region: row[3])
     end
   end
@@ -18,6 +23,22 @@ class Place < ApplicationRecord
     return if photo.blob.content_type.start_with? 'image/'
     photo.purge_later
     errors.add(:photo, 'needs to be an image')
+  end
+
+  def point_geojson
+    RGeo::GeoJSON.encode geojson
+  end
+
+  private
+
+  def geojson
+    RGeo::GeoJSON::Feature.new(
+      RGeo::Cartesian.factory.point(long, lat),
+      id,
+      name: name,
+      region: region,
+      stories: stories
+    )
   end
 
 end
