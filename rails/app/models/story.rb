35f758019b1c7d3280d5436a4d9d97f47462bbc4
Miranda Wang <mirandawang94@gmail.com>
2019-06-15 17:04:33 -0400

    Updating importers to match format

--- rails/app/models/story.rb
+++ rails/app/models/story.rb
@@ -8,14 +8,20 @@ class Story < ApplicationRecord
 
   def self.import_csv(filename)
     CSV.parse(filename, headers: true) do |row|
-      # todo: update point_id here
       perm = row[9].blank? ? "anonymous" : "user_only"
-      story = Story.create(title: row[0], point_id: 1)
-      speakers = Speaker.find_or_create_by(name: row[2])
-      story.speakers << speakers
+      story = Story.find_or_create_by(title: row[0], desc: row[1], permission_level: perm)
+      speaker_names = row[2]
+      speakers = []
+      speaker_names.split(',').each do |speaker|
+        story.speakers << Speaker.find_or_create_by(name: speaker.strip)
+      end
       # add place to collection that belongs to story
-      related_places = Place.find_or_create_by(name: row[3])
-      story.places << related_places
+      place_names = row[3]
+      place_names.split(',').each do |place|
+        story.places << Place.find_or_create_by(name: place.strip)
+      end
+      # Add interview location
+      story.interview_location = Place.find_or_create_by(name: row[4])
       if row[8] && File.exist?(Rails.root.join('media', row[8]))
         file = File.open(Rails.root.join('media',row[8]))
         story.media.attach(io: file, filename: row[8])
