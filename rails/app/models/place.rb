a8774285002962da301ba5ba0dac72c56a399342
Miranda Wang <mirandawang94@gmail.com>
2019-06-22 16:06:22 -0400

    Updating importers

--- rails/app/models/place.rb
+++ rails/app/models/place.rb
@@ -9,11 +9,15 @@ class Place < ApplicationRecord
 
   def self.import_csv(filename)
     CSV.parse(filename, headers: true) do |row|
-      place = Place.create(name: row[0], type_of_place: row[1], description: row[2], region: row[3], lat: row[5].to_f, long: row[4].to_f)
+      place = Place.find_or_create_by(name: row[0])
+      place.lat = row[5].to_f
+      place.long = row[4].to_f
+      place.type_of_place = row[1]
+      place.description = row[2]
+      place.region = row[3]
       if row[6] && File.exist?(Rails.root.join('media', row[6]))
         file = File.open(Rails.root.join('media',row[6]))
         place.photo.attach(io: file, filename: row[6])
-        place.save
       end
       place.save
     end
