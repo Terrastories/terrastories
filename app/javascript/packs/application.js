befd0bf9e7987d6cb056fa0542fea97c22b0c729
Kalimar Maia <kalimar.maia@gmail.com>
2019-09-18 11:51:40 -0400

    Fix webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,3 +8,6 @@
 // layout file, like app/views/layouts/application.html.erb
 
 console.log('Hello World from Webpacker')
+var componentRequireContext = require.context("components", true);
+var ReactRailsUJS = require("react_ujs");
+ReactRailsUJS.useContext(componentRequireContext);
