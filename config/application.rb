3c16bdffb47fe5caf3ca94f4672f67b6bb54b7f5
Robb Kidd <robb@thekidds.org>
2019-09-18 21:16:03 -0400

    remove the twiddle for web-console
    Because we don't use that in production. We SHOULDN'T use that in
    production.
    
    Signed-off-by: Robb Kidd <robb@thekidds.org>

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,6 @@ module App
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    config.web_console.permissions = '172.20.0.1'
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
