8e20481435e8aec5951ccfc698603d280d5fd5d9
lsfernandes92 <lsfernandes92@gmail.com>
2019-10-02 19:02:01 -0700

    Change translate name for the 'place_type'
    The task was to translate the 'place_type' and 'region' string from 
    marker popup from the map. It turns out that the texts was hard coded. I 
    found the key named 'select_place_type' in those files that was exactly 
    the translate I needed. So to not create another key for that I just 
    renamed the existed one. Also for the record that existed key was only 
    used for the story filter in the index page, so it did not interfered.

--- app/javascript/constants/FilterConstants.js
+++ app/javascript/constants/FilterConstants.js
@@ -1,1 +1,1 @@
-export const FILTER_CATEGORIES = [I18n.t("region"), I18n.t("select_place_type"), I18n.t("speaker")];
+export const FILTER_CATEGORIES = [I18n.t("region"), I18n.t("place_type"), I18n.t("speaker")];
