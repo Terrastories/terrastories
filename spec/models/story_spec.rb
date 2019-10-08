26eab4a9b1c9fd08edfc7e35509e57da6873738e
Midhun Krishna <kr1shn4.m1dhun@gmail.com>
2019-10-07 22:14:48 -0500

    287 Extracted out csv fata extraction logic into csv row decorator
    287 Added test coverage to story media csv decorator
    
    287 Complete addition test coverage with refactor and file reorganization

--- spec/models/story_spec.rb
+++ spec/models/story_spec.rb
@@ -1,5 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe Story, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'assocations' do
+    it { should have_many :speaker_stories }
+    it { should have_many :speakers }
+    it { should have_and_belong_to_many :places }
+
+    it { should belong_to :interview_location }
+    it { should belong_to :interviewer }
+  end
+
+  describe '.import_csv' do
+    it 'is tested against fixture file' do
+      expect(file_fixture('story_with_media.csv').read).not_to be_empty
+    end
+
+    it 'delegates all attributes to decorator' do
+      fixture_data = file_fixture('story_with_media.csv').read
+      described_class.import_csv(fixture_data)
+
+      story = described_class.last
+      csv = CSV.parse(fixture_data, headers: true).first
+
+      expect(story.title).to eq csv[0]
+      expect(story.desc).to eq csv[1]
+      expect(story.speakers.map(&:name).join(',')).to eq csv[2]
+      expect(story.places.map(&:name).join(',')).to eq csv[3]
+      expect(story.interview_location.name).to eq csv[4]
+      expect(story.date_interviewed.strftime('%m/%d/%y')).to eq csv[5]
+      expect(story.interviewer.name).to eq csv[6].strip
+      expect(story.language).to eq csv[7]
+      expect(story.permission_level).to eq "anonymous"
+      expect(story.media.first.filename.to_s).to eq csv[8]
+    end
+
+    it 'does not fail when media is not present' do
+      fixture_data = file_fixture('story_without_media.csv').read
+      described_class.import_csv(fixture_data)
+
+      story = described_class.last
+      csv = CSV.parse(fixture_data, headers: true).first
+
+      expect(story.media.all.count).to eq 0
+      expect(csv[8]).not_to be_nil
+    end
+  end
 end
