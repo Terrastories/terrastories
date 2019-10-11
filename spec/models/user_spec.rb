da5fd3b488440f5a4a39e3c5046d427ca988a394
Fedor Lukyanov <sleekybadger@gmail.com>
2019-10-12 00:53:25 +0300

    Add missing specs for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to define_enum_for(:role).with_values(%i[user editor]) }
+
+  describe 'Callbacks' do
+    describe 'after_initialize' do
+      subject(:user) { User.new(params) }
+
+      context 'with role' do
+        let(:params) { Hash[:role, :editor] }
+
+        it 'uses role from params' do
+          expect(user.role).to eq('editor')
+        end
+      end
+
+      context 'without role' do
+        let(:params) { Hash.new }
+
+        it 'sets default user role' do
+          expect(user.role).to eq('user')
+        end
+      end
+    end
+  end
 end
