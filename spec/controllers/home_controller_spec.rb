b6ff87b315a621b8a68a31613fe6432f4198ef3c
Radoslav <r.genchew@gmail.com>
2019-10-13 22:31:24 +0200

    HomeControllerSpec: add initial 'describe' blocks for #index, #show and #create actions

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,4 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
+  describe "GET index" do
+  end
+
+  describe "GET show" do
+  end
+
+  describe "POST create" do
+  end
 end
