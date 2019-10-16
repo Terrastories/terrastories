addf91e51a3518f8d9be5c2c5bd2c7234848e145
noelledusahel <barberossa@gmail.com>
2019-10-16 13:06:32 -0400

    add specs for place model

--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
+require 'csv'
+
 
 RSpec.describe Place, type: :model do
+
+
+  describe 'associations' do
+    it { should have_many :interview_stories }
+    it { should have_and_belongs_to_many :stories }
+  end
+
+  describe 'import_csv' do
+  end
+
+  describe '#photo_format' do
+    it 'should have a prefix' do
+      binding.pry
+    end
+  end
+
+  describe 'photo_url' do
+  end
+
+  describe 'point_geojson' do
+  end
+
   pending "add some examples to (or delete) #{__FILE__}"
 end
