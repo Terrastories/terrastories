ce220667fe0d8cef6e7838b0a21e05ea8830469a
kayhide <kayhide@gmail.com>
2019-10-25 14:46:19 +0900

    Fix users search on admin page

--- app/dashboards/user_dashboard.rb
+++ app/dashboards/user_dashboard.rb
@@ -11,7 +11,7 @@ class UserDashboard < Administrate::BaseDashboard
     id: Field::Number,
     email: Field::String,
     role: EnumField,
-    password: Field::String,
+    password: Field::String.with_options(searchable: false),
     created_at: Field::DateTime,
     updated_at: Field::DateTime,
   }.freeze
