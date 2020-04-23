5fd1ad8d057203228946e5294d634b34f8af0d7f
Sarah Laloggia <54509406+slaloggia@users.noreply.github.com>
2020-04-23 17:37:21 -0500

    Add validation tests for Story model - Test validation of speakers, through speaker_stories - Make interviewer and interview_location optional in association tests - Create traits for interviewer/interview_location and speakers in the story factory

--- rails/spec/factories/story_factory.rb
+++ rails/spec/factories/story_factory.rb
@@ -4,7 +4,19 @@ FactoryBot.define do
     desc  { "ACT program manager Rudo Kemper discusses why the organization decided to start building Terrastories to support local communities retain their oral history traditions." }
     language { 'English' }
     permission_level { 0 }
-    association :interviewer, factory: :speaker
-    association :interview_location, factory: :place
+
+    trait :with_interviewer do
+      after(:build) do |story|
+        story.interviewer << FactoryBot.build(:speaker)
+        story.interview_location << FactoryBot.build(:place)
+        
+      end
+    end
+
+    trait :with_speakers do
+      after(:build) do |story|
+        story.speakers = FactoryBot.build_list(:speaker, 2)
+      end
+    end
   end
 end
