c9e430f761cfc6157404862b74f34e6b8c01055b
Sarah Laloggia <54509406+slaloggia@users.noreply.github.com>
2020-04-23 15:27:21 -0500

    Remove unnecessary curriculum route

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,7 @@ Rails.application.routes.draw do
         post :import_csv
       end
     end
+
     devise_for :users, :controllers => { registrations: 'registrations' }
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root to: 'welcome#index'
