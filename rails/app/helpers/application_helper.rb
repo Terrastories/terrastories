f11b9212bc975bc29099115ca74e46cbd460f00a
Rudo Kemper <31662219+rudokemper@users.noreply.github.com>
2020-07-01 18:37:52 -0400

    446 translating selected phrase (#460)
    * Update pt.yml, nl.yml, mat.yml, en.yml, es.yml, ja.yml to remove parentheses from `language_select` and provide translation for Portuguese, Dutch and Matawai.
    
    * Update application_helper.rb to add parentheses here.

--- rails/app/helpers/application_helper.rb
+++ rails/app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def application_locales
     locales = I18n.available_locales.sort.map do |locale|
-      locale_identifier = locale == I18n.locale ? "#{locale} #{t("selected_language")} " : locale
+      locale_identifier = locale == I18n.locale ? "#{locale} (#{t("selected_language")}) " : locale
       link_to locale_identifier, root_path(locale: locale), class: "language-color"
     end
     safe_join(locales, " | ".html_safe)
