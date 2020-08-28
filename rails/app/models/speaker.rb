3078f91af9e00a2e0a7ec552d16a0d19f8a7d4a9
Laura Mosher <laura.trevisan@gmail.com>
2020-08-28 13:38:11 -0400

    Also update Speaker import_csv with import/media folder instead of just media

--- rails/app/models/speaker.rb
+++ rails/app/models/speaker.rb
@@ -1,7 +1,7 @@
 # == Schema Information ==
 #
 # Table name: speakers
-# 
+#
 # id          ... not null primary key
 # speaker_name... string
 # birth_year  ... datetime, nil if blank
@@ -33,7 +33,7 @@ class Speaker < ApplicationRecord
       speaker.birthplace = get_birthplace(row[2])
       # Assumes birth date field is always just a year
       speaker.birthdate = row[1].nil? || row[1].downcase == 'unknown' ? nil : Date.strptime(row[1], "%Y")
-      if row[3] && File.exist?(Rails.root.join('media', row[3]))
+      if row[3] && File.exist?(Rails.root.join('import/media', row[3]))
         file = File.open(Rails.root.join('media',row[3]))
         speaker.photo.attach(io: file, filename: row[3])
       end
