cce078fa4e82bcfa69aaad4adde5ca2df3b5ca83
Sarah Laloggia <54509406+slaloggia@users.noreply.github.com>
2020-09-27 13:48:14 -0500

    Nested has many test clean (#481)
    * Add functionality to Curriculum dashboard using NestedHasMany with CurriculumStories
    
    * Add story descriptions to the nested table in Curriculum dashboard
    
    * Fix display_resource method in Story dashboard
    
    * Quick fixes for breaking Curriculum forms:
    
    - Remove curriculum_stories from Form Attributes in dashboard
    - Add admin controller for CurriculumStories so that clicking Edit in the Show page for a curriculum doesn't break everything
    
    * Create custom form partial for curriculum
    
    * Create custom form partial for curriculum
    
    * Set admin form width to auto
    
    * Update administrate gem, admin navigation partial, and nested_has_many form partial
    
    * Create custom view for new curriculum
    
    * Customize attributes and styling for Curriculum forms and nested CurriculumStories form and table
    
    * Styling and default order for Curriculum dashboard and forms

--- rails/app/dashboards/curriculum_story_dashboard.rb
+++ rails/app/dashboards/curriculum_story_dashboard.rb
@@ -1,6 +1,6 @@
 require "administrate/base_dashboard"
 
-class CurriculumDashboard < Administrate::BaseDashboard
+class CurriculumStoryDashboard < Administrate::BaseDashboard
   # ATTRIBUTE_TYPES
   # a hash that describes the type of each of the model's fields.
   #
@@ -8,14 +8,13 @@ class CurriculumDashboard < Administrate::BaseDashboard
   # which determines how the attribute is displayed
   # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
-    user: Field::BelongsTo,
-    curriculum_stories: Field::NestedHasMany.with_options(skip: :curriculum, sort_by: :display_order),
-    stories: Field::HasMany,
+    curriculum: Field::BelongsTo,
+    story: Field::BelongsTo.with_options(searchable: true),
+    story_desc: Field::Text,
     id: Field::Number,
-    title: Field::String,
-    description: Field::Text,
     created_at: Field::DateTime,
     updated_at: Field::DateTime,
+    display_order: Field::Number,
   }.freeze
 
   # COLLECTION_ATTRIBUTES
@@ -24,32 +23,25 @@ class CurriculumDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
-  title
-  description
-  stories
-  user
+  story
+  story_desc
+  display_order
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = %i[
-  title
-  description
-  curriculum_stories
-  user
-  created_at
-  updated_at
+  curriculum
+  story
+  display_order
   ].freeze
 
   # FORM_ATTRIBUTES
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
-  user
-  title
-  description
-  stories
-  curriculum_stories
+  story
+  display_order
   ].freeze
 
   # COLLECTION_FILTERS
@@ -64,10 +56,14 @@ class CurriculumDashboard < Administrate::BaseDashboard
   #   }.freeze
   COLLECTION_FILTERS = {}.freeze
 
-  # Overwrite this method to customize how curriculums are displayed
+  # Overwrite this method to customize how curriculum stories are displayed
   # across all pages of the admin dashboard.
   #
-  # def display_resource(curriculum)
-  #   "Curriculum ##{curriculum.id}"
+  # def display_resource(curriculum_story)
+  #   "CurriculumStory ##{curriculum_story.id}"
   # end
+  def default_sorting_attribute
+    :display_order
+  end
+
 end
