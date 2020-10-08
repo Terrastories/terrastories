76e80ba23a059be978ea0b913ce2144fa6eb6928
tauhir <tauhir.edwards@gmail.com>
2020-10-08 22:50:16 +0200

    Sponsor theme tests
    Added active storage validator gem and updated tests

--- rails/spec/models/theme_spec.rb
+++ rails/spec/models/theme_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Theme, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:theme) {Theme.new()}
+  it "can add sponsor logo" do
+    theme.logos.attach(io: File.open("./spec/fixtures/media/terrastories.png"), filename: 'file.pdf')
+    theme.valid?
+    expect(theme.errors[:logos]).to be_empty
+  end
 end
