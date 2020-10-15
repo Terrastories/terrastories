9fa0b69006810b200b1f17948669a865e4979181
Radoslav <r.genchew@gmail.com>
2020-10-15 23:05:13 +0200

    CurriculumSpec: test accepting nested attributes for 'curriculum_stories'

--- rails/spec/models/curriculum_spec.rb
+++ rails/spec/models/curriculum_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Curriculum, type: :model do
   describe 'associations' do
-    it { should belong_to :user}
-    it { should have_many :curriculum_stories}
+    it { should belong_to :user }
+    it { should have_many :curriculum_stories }
     it { should have_many :stories }
+    it { should accept_nested_attributes_for(:curriculum_stories) }
   end
 end
