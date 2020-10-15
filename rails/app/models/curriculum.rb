c4b95d087931ebc98481fab70136f41ef2fc2c5b
Radoslav <r.genchew@gmail.com>
2020-10-15 23:04:42 +0200

    Curriculum: fix indentation and remove some empty lines

--- rails/app/models/curriculum.rb
+++ rails/app/models/curriculum.rb
@@ -1,8 +1,6 @@
 class Curriculum < ApplicationRecord
-    belongs_to :user
-    has_many :curriculum_stories
-    has_many :stories, through: :curriculum_stories
-    accepts_nested_attributes_for :curriculum_stories
-
-    
+  belongs_to :user
+  has_many :curriculum_stories
+  has_many :stories, through: :curriculum_stories
+  accepts_nested_attributes_for :curriculum_stories
 end
