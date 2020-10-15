e04e179dab7d77830b3996c92e6445045229f0d5
Radoslav <r.genchew@gmail.com>
2020-10-15 22:51:03 +0200

    MediaLinkSpec: test the association with 'story' model

--- rails/spec/models/media_link_spec.rb
+++ rails/spec/models/media_link_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe MediaLink, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do
+    it { should belong_to :story }
+  end
 end
