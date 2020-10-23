a8a2d779010ddb6836388b54df62cabb6204646c
Laura Mosher <laura.trevisan@gmail.com>
2020-10-23 11:56:41 -0400

    Add Community table
    In preparation for Terrastories multi-instance, this adds the new table
    to represent a Community in Terrastories.
    
    Currently, this table is standalone and has no relationships to other
    models in the application, though that will change as features are built
    out.

---
+++ rails/app/models/community.rb
@@ -0,0 +1,2 @@
+class Community < ApplicationRecord
+end
--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_26_232543) do
+ActiveRecord::Schema.define(version: 2020_10_23_155003) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,14 @@ ActiveRecord::Schema.define(version: 2020_07_26_232543) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "communities", force: :cascade do |t|
+    t.string "name"
+    t.string "locale"
+    t.string "country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "curriculum_stories", force: :cascade do |t|
