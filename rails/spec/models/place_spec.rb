9f2401c94a1eb4872abba31fa818f57669ce884e
Jaime Velaz <jaume.velaz@gmail.com>
2020-10-27 17:31:02 +0100

    #539 Improve spec readability

--- rails/spec/models/place_spec.rb
+++ rails/spec/models/place_spec.rb
@@ -11,19 +11,17 @@ RSpec.describe Place, type: :model do
       expect(file_fixture('place_with_media.csv').read).not_to be_empty
     end
 
-    describe "does not create same places multiple times" do
-      it "creates a single place" do
-        header, place = file_fixture('place_with_media.csv').read.split("\n")
-
-        @fixture_data = [
-          header,
-          place,
-          place,
-        ].join("\n")
+    describe "does not create duplicated places" do
+      it "creates a single place for same file twice" do
+        @fixture_data = file_fixture('place_with_media.csv').read
 
         expect {
           described_class.import_csv(@fixture_data)
         }.to change { Place.count }.by(1)
+
+        expect {
+          described_class.import_csv(@fixture_data)
+        }.to change { Place.count }.by(0)
       end
     end
 
