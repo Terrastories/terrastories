df8791f28e4bc0defd1f3f5f805a29be922bb880
Tauhir Edwards <tauhir.edwards@gmail.com>
2020-11-22 21:21:14 +0200

    557: Updated logo attribute and updates tests (#558)
    Changed logo to sponsor_logo to make the code clearer and updated the logo test (same outcome, less code!)

--- rails/app/models/theme.rb
+++ rails/app/models/theme.rb
@@ -1,6 +1,6 @@
 class Theme < ApplicationRecord
     
-    has_many_attached :logos
-    validates :logos, blob: { content_type: ['image/png', 'image/jpg', 'image/jpeg'], size_range: 1..5.megabytes }
+    has_many_attached :sponsor_logos
+    validates :sponsor_logos, blob: { content_type: ['image/png', 'image/jpg', 'image/jpeg'], size_range: 1..5.megabytes }
 
 end
