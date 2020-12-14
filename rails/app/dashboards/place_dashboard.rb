1e31f1c95c5988979d2c5e7a5ab15e65e9e4c317
Laura Mosher <laura.trevisan@gmail.com>
2020-12-13 21:07:40 -0500

    Improve places admin views

--- rails/app/dashboards/place_dashboard.rb
+++ rails/app/dashboards/place_dashboard.rb
@@ -28,21 +28,15 @@ class PlaceDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
-    :id,
+    :photo,
     :name,
     :description,
-    :type_of_place,
     :region,
-    :long,
-    :lat,
-    :stories,
-    :photo,
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = [
-    :id,
     :name,
     :description,
     :type_of_place,
