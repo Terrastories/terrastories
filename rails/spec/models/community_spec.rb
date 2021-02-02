a09c203ba5fc7d01a3473ee0b83b04218054a5f8
Laura Mosher <laura.trevisan@gmail.com>
2021-02-02 11:02:06 -0500

    Destroy associations on deletion.
    When a community is destroyed, destroy speakers, places, and stories
    associated with the Community.
    
    Nullify users relationship with the community.

--- rails/spec/models/community_spec.rb
+++ rails/spec/models/community_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Community, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do
+    let(:community) { FactoryBot.create(:community) }
+
+    it "destroys associated places" do
+      place = FactoryBot.create(:place, community: community)
+      other_place = FactoryBot.create(:place)
+
+      expect { community.destroy }.to change(Place, :count).by(-1)
+
+      expect(Place.all.pluck(:id)).to eq([other_place.id])
+    end
+
+    it "destroys associated speakers" do
+      speaker = FactoryBot.create(:speaker, community: community)
+      other_speaker = FactoryBot.create(:speaker)
+
+      expect { community.destroy }.to change(Speaker, :count).by(-1)
+
+      expect(Speaker.all.pluck(:id)).to eq([other_speaker.id])
+    end
+
+    it "destroys associated stories" do
+      speaker = FactoryBot.create(:speaker, community: community)
+      place = FactoryBot.create(:place, community: community)
+      story = FactoryBot.create(:story, community: community, speakers: [speaker], places: [place])
+      other_story = FactoryBot.create(:story, :with_speakers)
+
+      expect { community.destroy }.to change(Story, :count).by(-1)
+
+      expect(Story.all.pluck(:id)).to eq([other_story.id])
+      expect(Speaker.all.pluck(:id)).not_to include(speaker.id)
+      expect(Place.all.pluck(:id)).not_to include(place.id)
+    end
+  end
 end
