b079cad1bbc9105ead61bd9ebc51166a76fcd33c
Connie Chi <36419421+ConnieJChi@users.noreply.github.com>
2021-10-26 15:28:17 -0700

    687: Adding more typeahead fields (#701)
    * Adding speaker community as a typeahead field when editing/creating a speaker
    
    * Language form field is now a typeahead

--- rails/app/dashboards/story_dashboard.rb
+++ rails/app/dashboards/story_dashboard.rb
@@ -11,7 +11,7 @@ class StoryDashboard < Administrate::BaseDashboard
     id: Field::Number,
     title: Field::String,
     desc: Field::Text,
-    language: Field::String,
+    language: LanguageField,
     speakers: Field::ScopedHasMany.with_options(scope: -> (field) { field.resource.community.speakers }),
     places: Field::ScopedHasMany.with_options(scope: -> (field) { field.resource.community.places }),
     interview_location: Field::ScopedBelongsTo.with_options(class_name: "Place", scope: -> (field) { field.resource.community.places }),
