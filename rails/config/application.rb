2a5966f3872577c7526c63156d857447eae39136
Laura Mosher <laura.trevisan@gmail.com>
2022-04-07 14:43:41 -0400

    Update Rails 5.2.5 -> 5.2.7 (latest in 5.2 stable)
    Update gems

--- rails/config/application.rb
+++ rails/config/application.rb
@@ -20,5 +20,9 @@ module App
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*', '*.{rb,yml}').to_s]
     config.i18n.available_locales = Dir[Rails.root.join('config','locales', '*')].map {|f| File.basename(f).to_sym }
     config.i18n.default_locale = :en
+
+    # Upgrading from 5.2.5 requires this line in order to not break form submissions during deploy
+    # See: https://github.com/rails/rails/blob/5-2-stable/actionpack/CHANGELOG.md#rails-526-may-05-2021
+    config.action_controller.urlsafe_csrf_tokens = true
   end
 end
