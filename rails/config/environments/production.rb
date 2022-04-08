5c3a342dfb1e555ec7d8338dade099322958319d
Laura Mosher <laura.trevisan@gmail.com>
2022-04-08 16:50:45 -0400

    Address some of the TODOs and clean up from auto-gen files

--- rails/config/environments/production.rb
+++ rails/config/environments/production.rb
@@ -71,8 +71,7 @@ Rails.application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
-  # TODO: check if this should be "true" or if the array syntax is correct
-  config.i18n.fallbacks = [I18n.default_locale]
+  config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
