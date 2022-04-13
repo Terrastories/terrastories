7622a0985a7f2a3bea96996a4b024bcda442904f
rudokemper <rkemper@digital-democracy.org>
2022-04-13 16:17:57 -0400

    Add comment about internationalization in the future

--- rails/app/javascript/components/Popup.jsx
+++ rails/app/javascript/components/Popup.jsx
@@ -9,16 +9,15 @@ const Popup = (props) => {
         <h1>{name}</h1>
         <button className="ts-markerPopup-header-button" onClick={props.onCloseClick}>✕</button>
       </div>
-      // To do post-refactor: internationalize label strings in popup
+      {/* To do post-refactor: internationalize label strings in popup*/}
       <div className="ts-markerPopup-content">
-        {photo_url !== undefined && (<img src={photo_url} />)}
-        {name_audio_url !== undefined && (<div><span class="ts-markerPopup-label">Place name pronunciation:</span>
+        {photo_url && (<img src={photo_url} />)}
+        {name_audio_url && (<div><span class="ts-markerPopup-label">Place name pronunciation:</span>
                                                       <audio class="ts-markerPopup-audio"
                                                       controls
                                                       controlsList="nodownload"
                                                       src={name_audio_url}> </audio></div>)}
         {description && (<div class="ts-markerPopup-description">{description}</div>)}
-        {console.log(description)}
         {region && (<div><span class="ts-markerPopup-label">Region:</span> {region}</div>)}
         {type_of_place && (<div><span class="ts-markerPopup-label">Type of Place:</span> {type_of_place}</div>)}
       </div>
