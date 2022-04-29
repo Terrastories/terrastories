5affbde433d74038df5a19e5bdaddba0e798c1d0
rudokemper <rkemper@digital-democracy.org>
2022-04-29 10:07:38 -0400

    Change rails version in migration

--- rails/db/migrate/20220414120430_add_mapbox_3d_boolean.rb
+++ rails/db/migrate/20220414120430_add_mapbox_3d_boolean.rb
@@ -1,4 +1,4 @@
-class AddMapbox3dBoolean < ActiveRecord::Migration[6.0]
+class AddMapbox3dBoolean < ActiveRecord::Migration[5.2]
     def change
       add_column :themes, :mapbox_3d, :boolean, default: false
     end
