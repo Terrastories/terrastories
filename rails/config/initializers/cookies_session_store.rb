41f0a95392e65307d23aa228347869791bac25a7
Laura Mosher <laura.trevisan@gmail.com>
2022-06-10 16:15:19 -0400

    Save cookie session at root to be shared across multiple subdomains

---
+++ rails/config/initializers/cookies_session_store.rb
@@ -0,0 +1,4 @@
+Rails.application.config.session_store :cookie_store,
+                                       key: "_terrastories_session",
+                                       domain: :all,
+                                       tld_length: 2
