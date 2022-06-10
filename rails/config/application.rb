fff6ee5cfceb4b8e0d13e0c3c198407de3432744
Laura Mosher <laura.trevisan@gmail.com>
2022-06-10 16:14:43 -0400

    Disable evented file watcher && don't replace images on attach on many

--- rails/config/application.rb
+++ rails/config/application.rb
@@ -20,5 +20,8 @@ module App
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*', '*.{rb,yml}').to_s]
     config.i18n.available_locales = Dir[Rails.root.join('config','locales', '*')].map {|f| File.basename(f).to_sym }
     config.i18n.default_locale = :en
+
+    # Don't replace existing images on upload for has_many_attached
+    config.active_storage.replace_on_assign_to_many = false
   end
 end
--- rails/config/environments/development.rb
+++ rails/config/environments/development.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # Check if we use Docker to allow docker ip through web-console
   if File.file?('/.dockerenv') == true
