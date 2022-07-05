96b21ca0e554f9b74d7b4edeb61be1ff5b692358
Laura Mosher <laura@lauramosher.com>
2022-07-05 15:22:37 -0400

    Add create/update actions and views to Places

--- rails/app/controllers/dashboard/places_controller.rb
+++ rails/app/controllers/dashboard/places_controller.rb
@@ -15,8 +15,53 @@ module Dashboard
       end
     end
 
+    def new
+      authorize @place = community.places.new
+    end
+
+    def create
+      authorize Place
+      @place = community.places.new(place_params)
+
+      if @place.save
+        redirect_to @place
+      else
+        render :new
+      end
+    end
+
     def show
-      @place = community.places.find(params[:id])
+      @place = authorize community.places.find(params[:id])
+    end
+
+    def edit
+      @place = authorize community.places.find(params[:id])
+    end
+
+    def update
+      @place = authorize community.places.find(params[:id])
+
+      if @place.update(place_params)
+        redirect_to @place
+      else
+        render :edit
+      end
+    end
+
+    private
+
+    def place_params
+      params.require(:place).permit(
+        :name,
+        :description,
+        :photo,
+        :name_audio,
+        :type_of_place,
+        :region,
+        :lat,
+        :long,
+        story_ids: []
+      )
     end
   end
 end
