1ae5c251e875c2214c95bdd9ce69c729939d1883
Laura Mosher <laura@lauramosher.com>
2022-07-05 16:02:35 -0400

    Add Stories create/update

--- rails/app/controllers/dashboard/stories_controller.rb
+++ rails/app/controllers/dashboard/stories_controller.rb
@@ -15,8 +15,55 @@ module Dashboard
       end
     end
 
+    def new
+      authorize @story = community.stories.new
+    end
+
+    def create
+      authorize Story
+      @story = community.stories.new(story_params)
+
+      if @story.save
+        redirect_to @story
+      else
+        render :new
+      end
+    end
+
     def show
-      @story = community.stories.find(params[:id])
+      @story = authorize community.stories.find(params[:id])
+    end
+
+    def edit
+      @story = authorize community.stories.find(params[:id])
+    end
+
+    def update
+      @story = authorize community.stories.find(params[:id])
+
+      if @story.update(story_params)
+        redirect_to @story
+      else
+        render :edit
+      end
+    end
+
+    private
+
+    def story_params
+      params.require(:story).permit(
+        :title,
+        :desc,
+        :date_interviewed,
+        :language,
+        :permission_level,
+        :topic,
+        :interview_location_id,
+        :interviewer_id,
+        media: [],
+        speaker_ids: [],
+        place_ids: []
+      )
     end
   end
 end
