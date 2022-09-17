4b2e110c0dd4820391ab07de4c6ddb9734cea76b
Laura Mosher <laura@lauramosher.com>
2022-09-17 14:03:45 -0400

    Remove unnecessary initializer

--- rails/config/initializers/cookies_session_store.rb
+++
@@ -1,4 +0,0 @@
-# Rails.application.config.session_store :cookie_store,
-#                                        key: "_terrastories_session",
-#                                        domain: :all,
-#                                        tld_length: 2
