861401946acba2c141561bb7d9b4fbe547536bb5
Laura Mosher <laura@lauramosher.com>
2022-09-21 11:24:13 -0400

    Redirect super admin to new dashboard instead of administrate

--- rails/app/controllers/welcome_controller.rb
+++ rails/app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
   def index
     if current_user.super_admin
-      redirect_to admin_communities_path
+      redirect_to super_admin_root_path
     elsif current_community
       @theme = current_community.theme
     else
