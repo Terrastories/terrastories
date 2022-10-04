28df047381375a473e5a64231b465e487788f8c7
Laura Mosher <laura@mosher.tech>
2022-10-04 14:51:17 -0400

    Fix: Stories Filtering (#831)
    Ensure that StoryPage correctly filters habtm relationships so ALL
    relations are shown even if only matched on one.
    
    Essentially, rather than filtering from Story -> PlaceStory -> Place, we
    only go Story -> PlaceStory. Same for Speakers.

--- rails/app/pages/stories_page.rb
+++ rails/app/pages/stories_page.rb
@@ -12,8 +12,8 @@ class StoriesPage < Page
   def relation
     stories = @scoped_stories
 
-    stories = stories.joins(:places).where(places: {id: @meta[:place]}) if @meta[:place].present?
-    stories = stories.joins(:speakers).where(speakers: {id: @meta[:speaker]}) if @meta[:speaker].present?
+    stories = stories.joins(:places_stories).where(places_stories: {place_id: @meta[:place]}) if @meta[:place].present?
+    stories = stories.joins(:speaker_stories).where(speaker_stories: {speaker_id: @meta[:speaker]}) if @meta[:speaker].present?
     stories = stories.where(permission_level: @meta[:visibility]) if @meta[:visibility].present?
 
     stories.order(@meta[:sort_by] => @meta[:sort_dir])
