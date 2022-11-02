eaa358075168dc5267bd5afb99afcaec51423f68
Rudo Kemper <31662219+rudokemper@users.noreply.github.com>
2022-11-02 17:52:26 -0400

    add users to metrics controller

--- rails/app/controllers/super_admin/metrics_controller.rb
+++ rails/app/controllers/super_admin/metrics_controller.rb
@@ -5,7 +5,8 @@ module SuperAdmin
         places_count: Place.all.size,
         stories_count: Story.all.size,
         speakers_count: Speaker.all.size,
-        communities_count: Community.all.size
+        communities_count: Community.all.size,
+        users_count: User.all.size
       }
     end
   end
