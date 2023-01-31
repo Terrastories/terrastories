7862c620e3a82b548e5ec8088515b7f02ce674c7
Laura Mosher <lauramosher@users.noreply.github.com>
2023-01-30 20:57:39 -0500

    Allow Super Admins to modify their user record
    - Allows super admin to change their password
    - Allows super admin to update their username, email, photo, etc

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -5,10 +5,6 @@ Rails.application.routes.draw do
       root to: "stories#index", as: :member_root
 
       get :search, to: "search#index"
-      get :profile, to: "profile#edit"
-      patch :profile, to: "profile#update"
-      get :change_password, to: "passwords#edit"
-      post :change_password, to: "passwords#update"
 
       resource :community, only: [:show, :update], as: :community_settings do
         delete :background_img, action: :delete_background_img
@@ -46,6 +42,11 @@ Rails.application.routes.draw do
     end
 
     devise_for :users, :controllers => { registrations: 'registrations' }
+    get :profile, to: "profile#edit"
+    patch :profile, to: "profile#update"
+    get :change_password, to: "passwords#edit"
+    post :change_password, to: "passwords#update"
+
     root to: 'welcome#index'
     get 'home', to: 'home#index', as: "home_map"
     get "search", to: "home#community_search_index", as: "community_search"
