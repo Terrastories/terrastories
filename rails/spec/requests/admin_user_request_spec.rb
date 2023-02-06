76d54860d3e5179602099d109c43d2e7bb31a959
Garrett <gnjulaka@gmail.com>
2023-02-06 14:58:03 -0600

    Updated test with correct context and routing

--- rails/spec/requests/admin_user_request_spec.rb
+++ rails/spec/requests/admin_user_request_spec.rb
@@ -1,17 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "SuperAdmin user request", type: :request do
-  describe "GET home" do
-    context "a logged in user" do
-      let(:user) { FactoryBot.create(:user, role: 100, super_admin: true) }
+  describe "GET edit" do
+    context "with a super admin privileges" do
+      let(:super_admin) { FactoryBot.create(:user, role: 100, super_admin: true) }
+      let(:user) { FactoryBot.create(:user, role: 1) }
+
       before do
-        login_as user
+        login_as super_admin 
       end
 
-      it "test" do
-        p user
+      it "should return the user of the specified route" do
         get "/admin/users/#{user.id}/edit" 
 
+        p response
         expect(response).to have_http_status(:success)
       end
     end
