f536e0383a29eb68885c35b89b5cb6382edfc9e9
Laura Mosher <lauramosher@users.noreply.github.com>
2023-02-27 14:39:00 -0500

    Update CORS to only allow /api access to specific configured origins

--- rails/config/initializers/cors.rb
+++ rails/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:1080',/[a-z0-9]{4}-[0-9]{2}-[0-9]{3}-[0-9]{3}-[0-9]{3}.ngrok.io/
-    resource '*', headers: :any, methods: [:get]
+    origins ENV.fetch("CORS_ORIGINS", "").split(",").map { |origin| origin[0] == "\\" ? Regexp.new(origin) : origin }
+    resource '/api/*', headers: :any, methods: [:get]
   end
 end
