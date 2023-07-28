c22d74cfe99809044aa6fc9e53a5143faf384b02
Laura Mosher <lauramosher@users.noreply.github.com>
2023-07-28 15:17:18 -0400

    fix: only attempt to generate static map when access token is available
    This is mostly as a JustInCase measure; and especially for possible
    issues with local development.
    
    Before we only checked that a community had opted into a public
    interface. Now we also check that they have an access token set (either
    community level or default) through the theme method. And then in the
    event that the token is bad or expired or some other network issue
    occurred, we rescue any standard error when trying to IO stream the file
    and attach it and continue as if nothing bad happened.
    
    The static map is to ensure that our default provided mapbox API token
    is not accidentally innundated with requests on the loading screen of
    Explore where we might hit API rate limits.

--- rails/app/controllers/dashboard/communities_controller.rb
+++ rails/app/controllers/dashboard/communities_controller.rb
@@ -8,13 +8,17 @@ module Dashboard
       @community = authorize current_user.community
 
       @community.update(community_params)
-      if @community.public
+      if @community.public && @community.theme.mapbox_token.present?
         static_map_url = if @community.theme.boundaries
           "https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/#{@community.theme.boundaries.flatten.to_s.gsub(' ','').sub("[", "%5B").sub("]", "%5D")}/900x600?access_token=#{@community.theme.mapbox_token}"
         else
           "https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/#{@community.theme.center_long},#{@community.theme.center_lat},#{@community.theme.zoom},#{@community.theme.bearing},#{@community.theme.pitch}/900x600?access_token=#{@community.theme.mapbox_token}"
         end
-        @community.theme.static_map.attach(io: URI.parse(static_map_url).open, filename: "#{@community.slug}-static-map.png")
+        begin
+          @community.theme.static_map.attach(io: URI.open(static_map_url), filename: "#{@community.slug}-static-map.png")
+        rescue StandardError
+          # do nothing; no worries if we can't save the map
+        end
       end
 
       redirect_to community_settings_path
