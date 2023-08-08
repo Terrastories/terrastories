4a7896df1492d15bb63ba2908138c33985b8108b
Laura Mosher <lauramosher@users.noreply.github.com>
2023-08-08 14:48:32 -0400

    Enable ActiveStorage 6.1 defaults

--- rails/config/initializers/new_framework_defaults_6_1.rb
+++ rails/config/initializers/new_framework_defaults_6_1.rb
@@ -48,10 +48,10 @@
 # Rails.application.config.action_view.form_with_generates_remote_forms = false
 
 # Set the default queue name for the analysis job to the queue adapter default.
-# Rails.application.config.active_storage.queues.analysis = nil
+Rails.application.config.active_storage.queues.analysis = nil
 
 # Set the default queue name for the purge job to the queue adapter default.
-# Rails.application.config.active_storage.queues.purge = nil
+Rails.application.config.active_storage.queues.purge = nil
 
 # Set the default queue name for the incineration job to the queue adapter default.
 # Rails.application.config.action_mailbox.queues.incineration = nil
