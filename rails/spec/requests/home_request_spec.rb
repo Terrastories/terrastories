bcd964d2106af976d76769ae583d44a6a4b17aae
Isaac Hudson <ice1080@gmail.com>
2023-10-05 21:15:25 -0700

    issue 956 - unit test

--- rails/spec/requests/home_request_spec.rb
+++ rails/spec/requests/home_request_spec.rb
@@ -20,5 +20,51 @@ RSpec.describe "Home request", type: :request do
         expect(response).to redirect_to("/users/sign_in")
       end
     end
+
+    context "orphaned stories" do
+      let(:community) { FactoryBot.create(:community, name: "Orphaned Community") }
+      let(:user) { FactoryBot.create(:user, community: community) }
+      let!(:place_1) { create(:place, community: community, region: "test place 1") }
+      let!(:place_2) { create(:place, community: community, region: "test place 2") }
+      let!(:speaker_1) { create(:speaker, community: community) }
+      let!(:speaker_2) { create(:speaker, community: community) }
+      let!(:story_1) do
+        create(
+          :story,
+          community: community,
+          places: [place_1],
+          speakers: [speaker_1],
+          desc: "This story should not be visible because it has no speaker",
+          permission_level: :anonymous
+        )
+      end
+      let!(:story_2) do
+        create(
+          :story,
+          community: community,
+          places: [place_2],
+          speakers: [speaker_2],
+          desc: "This story should not be visible because it has no place",
+          permission_level: :anonymous
+        )
+      end
+
+      def json_response
+        JSON.parse(response.body)
+      end
+
+      before do
+        login_as user
+      end
+
+      it "excludes stories with no speaker and/or place" do
+        speaker_1.destroy
+        place_2.destroy
+        get "/home"
+
+        expect(response.body).not_to include('This story should not be visible because it has no speaker')
+        expect(response.body).not_to include('This story should not be visible because it has no place')
+      end
+    end
   end
 end
