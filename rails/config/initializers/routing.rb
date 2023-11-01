b4d24750a4fb579032b65abeaf3b96aef189ab90
Laura Mosher <lauramosher@users.noreply.github.com>
2023-11-01 17:14:49 -0400

    Update Docker configuration so shakapacker compiles assets for production-ish environments

--- rails/config/initializers/routing.rb
+++ rails/config/initializers/routing.rb
@@ -3,8 +3,7 @@
 Rails.application.routes.default_url_options[:host] = ENV.fetch("HOST_HOSTNAME", "localhost")
 Rails.application.routes.default_url_options[:protocol] = Rails.env.production? ? "https" : "http"
 
-# NOTE(LM): Heroku doesn't allow routing through port.
-# So for now, disabling PORT if env is production.
-unless Rails.env.production?
+# NOTE(LM): Only set PORT in development
+if Rails.env.development?
   Rails.application.routes.default_url_options[:port] = ENV.fetch("PORT", 3000)
 end
