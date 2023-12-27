9bf8a7140979b58b64c1ee27c072c8a213abf67b
Laura Mosher <lauramosher@users.noreply.github.com>
2023-12-27 16:00:00 -0500

    Update CORS to always include HOST_HOSTNAME

--- rails/config/initializers/cors.rb
+++ rails/config/initializers/cors.rb
@@ -1,6 +1,14 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  cors = ENV.fetch("CORS_ORIGINS", "").split(",")
+
+  # This ensures that Explore can access TS locally when running with a custom hostname
+  if ENV["HOST_HOSTNAME"]
+    cors << ENV["HOST_HOSTNAME"]
+    cors << ENV["HOST_HOSTNAME"] + ":1080"
+  end
+
   allow do
-    origins ENV.fetch("CORS_ORIGINS", "").split(",").map { |origin| origin[0] == "\\" ? Regexp.new(origin) : origin }
+    origins cors.map { |origin| origin[0] == "\\" ? Regexp.new(origin) : origin }
     resource '/api/*', headers: :any, methods: [:get]
     resource '/rails/active_storage/*', headers: :any, methods: [:get]
   end
