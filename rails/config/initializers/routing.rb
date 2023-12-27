eaf34a52cc0fdd89146adbc231a0548beaf527de
Laura Mosher <lauramosher@users.noreply.github.com>
2023-12-27 15:58:11 -0500

    Set protocol based on env
    - https on production
    - http on dev/offline/test

--- rails/config/initializers/routing.rb
+++ rails/config/initializers/routing.rb
@@ -1,6 +1,7 @@
 # Define default URL options for rails url helpers
 #
 Rails.application.routes.default_url_options[:host] = ENV.fetch("HOST_HOSTNAME", "localhost")
+Rails.application.routes.default_url_options[:protocol] = Rails.env.production? ? "https" : "http"
 
 # NOTE(LM): Only set PORT in development
 if Rails.env.development?
